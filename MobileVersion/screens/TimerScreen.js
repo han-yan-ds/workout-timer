d001555f1a7dc8e9631872dcd5a6232763b08e05
han-yan-ds <hanprogramming@gmail.com>
2019-10-25 15:34:12 -0600

    Got both screens to render properly on app

--- MobileVersion/screens/TimerScreen.js
+++ MobileVersion/screens/TimerScreen.js
@@ -3,7 +3,7 @@ import {
   Text,
   View
 } from 'react-native';
-import Timer from '../components/Timer/Timer.jsx'
+import WorkoutTimer from '../components/Timer/WorkoutTimer.jsx'
 
 import styles from '../styles/screenStyles';
 
@@ -11,7 +11,7 @@ function TimerScreen() {
   return (
     <View style={styles.screenContainer}>
       <Text style={styles.screenText}>
-        <Timer />
+        <WorkoutTimer />
       </Text>
     </View>
   );
