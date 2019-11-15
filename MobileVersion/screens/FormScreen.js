ec1ef48e45f5af5bd7230d2efbdb563f7499effd
han-yan-ds <hanprogramming@gmail.com>
2019-11-15 11:01:49 -0700

    Switched Form and Timer order

--- MobileVersion/screens/FormScreen.js
+++ MobileVersion/screens/FormScreen.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Text,
   View
 } from 'react-native';
 import Form from '../components/Form/Form.jsx'
+import { withNavigationFocus } from 'react-navigation';
 
 import styles from '../styles/screenStyles';
 
-function FormScreen() {
-  return (
-    <View style={styles.screenContainer}>
-      <Text style={styles.screenText}>
-        <Form />
-      </Text>
-    </View>
-  );
+class FormScreen extends Component {
+  
+  componentDidUpdate(prevProps) {
+    if (!this.props.isFocused && prevProps.isFocused) { // change to Timer focus
+      // generate finalWorkout to timer
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.screenContainer}>
+        <Text style={styles.screenText}>
+          <Form />
+        </Text>
+      </View>
+    );
+  }
 }
 
-export default FormScreen;
+export default withNavigationFocus(FormScreen);
