9a271bf01693a576672816ac228af0b95939c6b8
han-yan-ds <hanprogramming@gmail.com>
2020-06-20 09:11:12 -0600

    Added a few browser interaction tests

--- WebVersion/cypress/integration/browser-interactions.spec.js
+++ WebVersion/cypress/integration/browser-interactions.spec.js
@@ -1,7 +1,19 @@
 /// <reference types="cypress" />
 
-describe('Opening a webpage', () => {
-  it("should load correct url", () => {
-    cy.visit("https://www.google.com", { timeout: 10000 }) // timeout parameter is a time limit for the page to load
+describe('Opening Example.com', () => {
+  it("should load correct url in a reasonable amount of time", () => {
+    cy.visit("https://www.example.com", { timeout: 10000 }) // timeout parameter is a time limit for the page to load
+  })
+
+  it("should check correct url", () => {
+    cy.url().should('include', 'example.com') //cy.url()
+  })
+
+  it('should check for correct element on the page', () => {
+    cy.get('h1').should('be.visible')
+  })
+
+  it('should figure out if the page contains certain text', () => {
+    cy.contains('illustrative')
   })
 })
